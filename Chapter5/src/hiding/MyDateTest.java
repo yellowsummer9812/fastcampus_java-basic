c1af4c75d41f803eb6490ae12fd39f8f67e2a304
yellowsummer <cshee9812@gmail.com>
2021-01-05 19:42:52 +0900

    oop 9강,10강 : 정보 은닉

---
+++ Chapter5/src/hiding/MyDateTest.java
@@ -0,0 +1,16 @@
+package hiding;
+
+public class MyDateTest {
+
+	public static void main(String[] args) {
+
+		MyDate date = new MyDate();
+		
+		date.setYear(2021);
+		date.setMonth(01);
+		date.setDay(77);
+		
+		date.showDate();
+	}
+
+}
