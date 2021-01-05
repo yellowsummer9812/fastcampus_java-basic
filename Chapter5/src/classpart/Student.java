79643d5823b9f887977e0cb8d1226af2661ca80f
yellowsummer <cshee9812@gmail.com>
2021-01-05 17:35:07 +0900

    oop 7강 Student 클래스 수정

--- Chapter5/src/classpart/Student.java
+++ Chapter5/src/classpart/Student.java
@@ -7,6 +7,16 @@ public class Student { // 한 JAVA파일 안에 public은 하나만 있어야한다. 파일 이름
 	public String studentName;
 	public String address;
 	
+	public Student (String name) {
+		studentName = name;
+	}
+	
+	public Student(int id, String name) {
+		studentID = id;
+		studentName = name;
+		address = "주소없음";
+	}
+	
 	// 메서드
 	public void showStudentInfo() {
 		System.out.println(studentName + "," + address);
--- Chapter5/src/classpart/StudentTest.java
+++ Chapter5/src/classpart/StudentTest.java
@@ -4,21 +4,21 @@ public class StudentTest {
 
 	public static void main(String[] args) {
 		
-		Student studentLee = new Student(); // 생성자로 객체 생성
-		studentLee.studentName = "이순신";
+		Student studentLee = new Student("이순신"); 
+		//studentLee.studentName = "이순신";
 		studentLee.address = "서울";
 		
 		studentLee.showStudentInfo();
 		
-		Student studentKim = new Student();
-		studentKim.studentName = "김영태";
+		Student studentKim = new Student(2, "김영태");
+		//studentKim.studentName = "김영태";
 		studentKim.address = "서울";
 		
 		studentKim.showStudentInfo();
 		
 		// 참조 변수를 직접 출력해보면 참조 변수가 가리키고있는 주소값을 알 수 있다.
-		System.out.println(studentLee);
-		System.out.println(studentKim);
+		//System.out.println(studentLee);
+		//System.out.println(studentKim);
 	}
 
 }
