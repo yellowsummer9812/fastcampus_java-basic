58c73f607e7e5633c2ae3acca15b7a574d276991
yellowsummer <cshee9812@gmail.com>
2021-01-08 21:48:46 +0900

    oop 29,30강 : 다형성

---
+++ Chapter7/src/polymorphism/VIPCustomer.java
@@ -0,0 +1,26 @@
+package polymorphism;
+
+public class VIPCustomer extends Customer{
+
+	double salesRatio;
+	private int agentID;
+	
+	public VIPCustomer(int customerID, String customerName) {
+		
+		super(customerID,customerName); // 상위클래스 생성자 명시적 호출
+		customerGrade = "VIP";
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+		
+		//System.out.println("VIPCustomer(int, String) 생성자 호출");
+	}
+
+	@Override
+	public int calcPrice(int price) {
+		
+		bonusPoint += price * bonusRatio;
+		return price - (int)(price * salesRatio);
+	}
+	
+	
+}
