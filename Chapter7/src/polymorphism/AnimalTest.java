da300f6c1252a3dc688b0b7bde4d383e7ce51bc7
yellowsummer <cshee9812@gmail.com>
2021-01-08 22:09:50 +0900

    oop 31강 : 다운캐스팅과 instanceof

--- Chapter7/src/polymorphism/AnimalTest.java
+++ Chapter7/src/polymorphism/AnimalTest.java
@@ -12,17 +12,29 @@ class Human extends Animal{
 	public void move() {
 		System.out.println("사람이 두 발로 걷습니다.");
 	}
+	
+	public void readBooks() {
+		System.out.println("사람이 책을 읽습니다.");
+	}
 }
 
 class Tiger extends Animal{
 	public void move() {
 		System.out.println("호랑이가 네 발로 뜁니다.");
 	}
+	
+	public void hunting() {
+		System.out.println("사냥을 합니다.");
+	}
 }
 
 class Eagle extends Animal{
 	public void move() {
 		System.out.println("독수리가 하늘을 날아갑니다.");
+	}
+	
+	public void flying() {
+		System.out.println("독수리가 날개를 쭉 펴고 멀리 날아갑니다.");
 	}
 }
 
@@ -33,6 +45,14 @@ public class AnimalTest {
 		Animal hAnimal = new Human();
 		Animal tAnimal = new Tiger();
 		Animal eAnimal = new Eagle();	
+		
+		/*
+		Eagle human = (Eagle)hAnimal; -> 오류 발생
+		if( hAnimal instanceof Human) {
+			Human human = (Human)hAnimal;
+			human.readBooks();
+		}
+		*/
 		
 		/*
 		AnimalTest test = new AnimalTest();
@@ -46,8 +66,32 @@ public class AnimalTest {
 		animalList.add(tAnimal);
 		animalList.add(eAnimal);
 		
+		AnimalTest test = new AnimalTest();
+		test.testDownCasting(animalList);
+		
+		/*
 		for(Animal animal : animalList) {
 			animal.move();
+		}
+		*/
+	}
+	
+	public void testDownCasting(ArrayList<Animal> list) {
+		for(int i = 0; i < list.size(); i++) {
+			Animal animal = list.get(i);
+			
+			if( animal instanceof Human) {
+				Human human = (Human)animal;
+				human.readBooks();
+			}else if( animal instanceof Tiger) {
+				Tiger tiger = (Tiger)animal;
+				tiger.hunting();
+			}else if( animal instanceof Eagle) {
+				Eagle eagle = (Eagle)animal;
+				eagle.flying();
+			}else {
+				System.out.println("error");
+			}
 		}
 	}
 	
