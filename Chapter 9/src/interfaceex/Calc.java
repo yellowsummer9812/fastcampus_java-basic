f9f94e297d63b33f2e99425b30fc65507023b685
yellowsummer <cshee9812@gmail.com>
2021-01-12 15:34:08 +0900

    oop 37강 : 인터페이스

---
+++ Chapter 9/src/interfaceex/Calc.java
@@ -0,0 +1,12 @@
+package interfaceex;
+
+public interface Calc {
+	
+	double PI = 3.14;
+	int ERROR = -999999999;
+	
+	int add(int num1, int num2);
+	int substract(int num1, int num2);
+	int times(int num1, int num2);
+	int devide(int num1, int num2);
+}
