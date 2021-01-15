97eecb0907164932af453fea527620f1f6e93ab2
yellowsummer <cshee9812@gmail.com>
2021-01-15 17:57:10 +0900

    oop 63, 64강 : 예외와 예외 처리

---
+++ Chapter 13/src/exception/ExceptionTest2.java
@@ -0,0 +1,21 @@
+package exception;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ExceptionTest2 {
+
+	public static void main(String[] args) {
+
+		
+		try(FileInputStream fis = new FileInputStream("a.txt")){
+			
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+
+}
