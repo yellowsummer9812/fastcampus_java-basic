8a3ad2f07a183cab6ec44ff47682a00bf855193f
yellowsummer <cshee9812@gmail.com>
2021-01-15 22:50:29 +0900

    oop 67강 : 표준 입출력

---
+++ Chapter 14/src/stream/SystemInTest2.java
@@ -0,0 +1,26 @@
+package stream;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SystemInTest2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("입력 후 '끝'이라고 쓰세요 : ");
+		
+		try {
+			int i;
+			// 한 바이트씩을 인식하는 애를 multi byte를 읽을 수 있게 해주기 위해 보조스트림 InputStreamReader 사용
+			// 바이트로 읽어들인 것을 문자로 바꿔줌
+			InputStreamReader isr = new InputStreamReader(System.in);
+			while( (i = isr.read()) != '끝') {
+				System.out.print((char) i);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
